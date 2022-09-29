f878aae596bded41a637944b722e795e8c5bd6b9
attah ifeanyi <attahsixtus6@gmail.com>
2022-09-29 18:11:38 +0100

    Page Navigation and Matching Strategy

--- script.js
+++ script.js
@@ -44,3 +44,15 @@ btnScrollTo.addEventListener('click', function (e) {
   e.preventDefault(e);
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+///////////////////////////////////////////////////////////////
+// Page Navigation //
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
